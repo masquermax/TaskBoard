644fb67f137daaca34113e18deaa63ecc46b6108
masquermax <224842982+masquermax@users.noreply.github.com>
2026-08-17 15:05:41 +0800

    feat: expose extension author api surface

---
+++ src/extensions/public-api.js
@@ -0,0 +1,5 @@
+export { EXTENSION_API_VERSION, OrchestrationMode } from './runtime/extension-registry.js';
+export { ExecutorPort } from '../core/executor-port.js';
+export { CapabilityProviderPort, DiscoveryLevel, basicCapabilitySnapshot } from './ports/capability-provider.js';
+export { ConnectionSettingsPort } from './ports/connection-settings.js';
+export { SurfaceHostPort } from './ports/surface-host.js';
