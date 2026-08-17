86073e9bf6516871187061bb0ec712a9beb00c56
masquermax <224842982+masquermax@users.noreply.github.com>
2026-08-17 18:57:17 +0800

    feat: expose continuation host contract

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -3,4 +3,5 @@ export { createBuiltinExtensionRegistry } from './builtins/index.js';
 export { ExecutorPort } from '../core/executor-port.js';
 export { CapabilityProviderPort, DiscoveryLevel, basicCapabilitySnapshot } from './ports/capability-provider.js';
 export { ConnectionSettingsPort } from './ports/connection-settings.js';
+export { ContinuationPort } from './ports/continuation.js';
 export { SurfaceHostPort } from './ports/surface-host.js';
