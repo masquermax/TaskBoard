96717038e6a61197d918218fd972296b06f02d5d
masquermax <224842982+masquermax@users.noreply.github.com>
2026-08-17 13:48:05 +0800

    feat: expose extension composition entry point

---
+++ src/extensions/index.js
@@ -0,0 +1,5 @@
+export { ExtensionRegistry, OrchestrationMode } from './runtime/extension-registry.js';
+export { createBuiltinExtensionRegistry } from './builtins/index.js';
+export { ExecutorPort } from '../core/executor-port.js';
+export { CapabilityProviderPort, DiscoveryLevel, basicCapabilitySnapshot } from './ports/capability-provider.js';
+export { SurfaceHostPort } from './ports/surface-host.js';
