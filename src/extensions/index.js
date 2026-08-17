2ed0f0b736b6136023b30e654068c1b679d35a87
masquermax <224842982+masquermax@users.noreply.github.com>
2026-08-17 15:05:51 +0800

    feat: expose extension api version on host surface

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -1,4 +1,4 @@
-export { ExtensionRegistry, OrchestrationMode } from './runtime/extension-registry.js';
+export { ExtensionRegistry, EXTENSION_API_VERSION, OrchestrationMode } from './runtime/extension-registry.js';
 export { createBuiltinExtensionRegistry } from './builtins/index.js';
 export { ExecutorPort } from '../core/executor-port.js';
 export { CapabilityProviderPort, DiscoveryLevel, basicCapabilitySnapshot } from './ports/capability-provider.js';
