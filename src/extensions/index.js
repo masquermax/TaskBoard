989c5488f1203b01d6052e9cda16c3b4cb48d2ca
masquermax <224842982+masquermax@users.noreply.github.com>
2026-08-19 23:13:18 +0800

    fix: remove builtin extensions from public host api

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -1,5 +1,4 @@
 export { ExtensionRegistry, EXTENSION_API_VERSION, OrchestrationMode } from './runtime/extension-registry.js';
-export { createBuiltinExtensionRegistry } from './builtins/index.js';
 export { ExecutorPort } from '../core/executor-port.js';
 export { CapabilityProviderPort, DiscoveryLevel, basicCapabilitySnapshot } from './ports/capability-provider.js';
 export { ConnectionSettingsPort } from './ports/connection-settings.js';
